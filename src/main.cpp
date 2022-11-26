93a7e042299fb303ba98d30b428b853f19767cbd
Robert May <robert.may@protonmail.com>
2022-11-25 18:12:55 -0700

    Added Menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <psp2/kernel/processmgr.h>
 #include <psp2/ctrl.h>
+#include <psp2/kernel/clib.h>
 #include <sstream>
 #include <vector>
 #include <cstdio>
 #include <SDL2/SDL.h>
 #include <algorithm>
+#include <string>
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include <stb_truetype.h>
 
 //Screen dimension constants
 enum {
@@ -91,20 +96,98 @@ bool RectangleCircleIntersection(const Rect& rect, const Circle& circle)
 	return (cornerDistance_sq <= (circle.r * circle.r));
 }
 
-int main(int argc, char *argv[]) 
+enum GameMode
 {
-	if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER ) < 0 )
-		return -1;
+	Menu,
+	Game, 
+	Victory
+};
 
-	if ((gWindow = SDL_CreateWindow( "RedRectangle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN)) == NULL)
-		return -1;
+struct float2
+{
+	float x;
+	float y;
+};
 
-	if ((gRenderer = SDL_CreateRenderer( gWindow, -1, 0)) == NULL)
-		return -1;
+struct float3
+{
+	float x;
+	float y;
+	float z;
+};
 
-	SDL_GameController* controller1 = SDL_GameControllerOpen(0);
-	printf("Hello: %i\n", (int)controller1 );
 
+struct FontAsset
+{
+	SDL_Texture* 	textures[256];
+	float3			wh[256];
+};
+
+struct GameState
+{
+	GameMode 	mode;
+	FontAsset	defaultFont;
+};
+
+void LoadFont(GameState& state)
+{
+	long size;
+	unsigned char* fontBuffer;
+	stbtt_fontinfo font;
+
+	FILE* fontFile = fopen("font.ttf", "rb");
+
+	fseek(fontFile, 0, SEEK_END);
+	size = ftell(fontFile); /* how long is the file ? */
+	fseek(fontFile, 0, SEEK_SET); /* reset */
+    
+	fontBuffer = (unsigned char*)malloc(size);
+
+	fread(fontBuffer, size, 1, fontFile);
+	fclose(fontFile);
+
+	if (!stbtt_InitFont(&font, fontBuffer, 0))
+	{
+		printf("failed\n");
+	}
+
+	int b_w = 128; /* bitmap width */
+	int b_h = 32; /* bitmap height */
+	int l_h = 64; /* line height */
+	int baseline = 64; 
+
+	struct RGBA
+	{
+		unsigned char R, G, B, A;
+	};
+
+	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for(size_t I = 0; I < sizeof(characters); I++)
+	{
+		char c = characters[I];
+
+		auto fontBitmap = stbtt_GetCodepointBitmap(&font, 0,stbtt_ScaleForPixelHeight(&font, 64), c, &b_w, &b_h, 0, &baseline);
+		RGBA* bitmap = (RGBA*)malloc(sizeof(RGBA) * b_w * b_h);
+
+		for (size_t I = 0; I < b_w * b_h; I++)
+			bitmap[I] = RGBA{ fontBitmap[I], fontBitmap[I], fontBitmap[I], fontBitmap[I]};
+
+		SDL_Texture* texture = SDL_CreateTexture(gRenderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, b_w, b_h);
+		SDL_UpdateTexture(texture, nullptr, bitmap, b_w * 4);
+		SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+
+		state.defaultFont.textures[c] 	= texture;
+		state.defaultFont.wh[c] 		= { b_w, b_h, baseline };
+
+		free(fontBitmap);
+		free(bitmap);
+	}	
+
+}
+
+void PlayState(SDL_GameController* controller1, GameState& state)
+{
 	const size_t paddleHeight	= 50;
 	const size_t paddleWidth 	= 100;
 
@@ -122,7 +205,6 @@ int main(int argc, char *argv[])
 
 	std::vector<Rect> blocks;
 	std::vector<FallingRect> fallingBlocks;
-
 
 	for(size_t I = 0; I < 10; I++)
 		blocks.push_back(Rect{ blockStepX + blockStepX * I, 50, blockStepX - 10, 50 });
@@ -145,10 +227,8 @@ int main(int argc, char *argv[])
 		paddleX = std::max(0.0f, paddleX);
 		paddleX = std::min(float(SCREEN_WIDTH - paddleWidth), paddleX);
 
-		SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0);
+		SDL_SetRenderDrawColor(gRenderer, 0xF1, 0xD3, 0xB3, 0xff);
 		SDL_RenderClear(gRenderer);
-
-		SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 255);
 
 		const int x = (int)(100 * cos(t / 10.0f));
 		const int y = (int)(100 * sin(t / 10.0f));
@@ -182,7 +262,6 @@ int main(int argc, char *argv[])
 			ballY_V = -ballY_V;
 		}
 
-		SDL_RenderFillRect(gRenderer, &paddleRect);
 
 		for (auto& block : fallingBlocks)
 		{
@@ -203,8 +282,8 @@ int main(int argc, char *argv[])
 			if (RectangleCircleIntersection(block, Circle { ballX, ballY, 50.0f }))
 			{
 				FallingRect fallingBlock;
-				fallingBlock.rect = block;
-				fallingBlock.v = 0.0f;
+				fallingBlock.rect 	= block;
+				fallingBlock.v 		= 0.0f;
 				fallingBlocks.push_back(fallingBlock);
 			}
 		}
@@ -217,13 +296,23 @@ int main(int argc, char *argv[])
 						   }),
 			fallingBlocks.end());
 
-		blocks.erase(
-			std::remove_if(blocks.begin(), blocks.end(), 
-			               [&](Rect& block) -> bool
-			               { 
-			               	return RectangleCircleIntersection(block, Circle	{ ballX, ballY, 50.0f });  
-						   }),
-			blocks.end());
+		
+		auto intersection_end = std::remove_if(blocks.begin(), blocks.end(), 
+									[&](Rect& block) -> bool
+									{ 
+											return RectangleCircleIntersection(block, Circle{ ballX, ballY, 50.0f });  
+									});
+
+		
+		for (auto I = intersection_end; I < blocks.end(); I++)
+		{
+			ballX_V *= 1.05f;
+			ballY_V *= 1.05f;
+		}
+
+		blocks.erase(intersection_end, blocks.end());
+
+		SDL_SetRenderDrawColor(gRenderer, 0x8B, 0X7E, 0X74, 255);
 
 		for (auto& block : blocks)
 		{
@@ -236,6 +325,8 @@ int main(int argc, char *argv[])
 
 			SDL_RenderFillRect(gRenderer, &blockRect);
 		}
+
+		SDL_SetRenderDrawColor(gRenderer, 0xC7, 0XBC, 0XA1, 255);
 
 		for (auto& block : fallingBlocks)
 		{
@@ -251,7 +342,10 @@ int main(int argc, char *argv[])
 
 		printf("Ball Y Velocity: %i\n", (int)ballY_V);
 
-		DrawCircle(gRenderer, ballX, ballY, 50, 255, 0, 255);
+		DrawCircle(gRenderer, ballX, ballY, 50.0f, 0x65, 0x64, 0x7c);
+
+		SDL_SetRenderDrawColor(gRenderer, 0x61, 0x76, 0x4b, 255);
+		SDL_RenderFillRect(gRenderer, &paddleRect);
 
 		SDL_RenderPresent(gRenderer);
 		SDL_Delay(16);
@@ -263,6 +357,141 @@ int main(int argc, char *argv[])
 		printf("Player Lost!\n");
 	else
 		printf("Player Win!\n");
+
+	state.mode = Victory;
+}
+
+template<typename TY>
+TY Clamp(const TY min, const TY x, const TY max) noexcept
+{
+	return std::max(std::min(x, max), min);
+}
+
+void DrawButton(const int x, const int y, const int w, const int h, const std::string& text, const SDL_Color& buttonColor, FontAsset& font)
+{
+	const SDL_Rect btnRect = { x, y, w, h };
+
+	SDL_SetRenderDrawColor(gRenderer, buttonColor.r, buttonColor.g, buttonColor.b, buttonColor.a);
+	SDL_RenderFillRect(gRenderer, &btnRect);
+
+	const float textCharacterWidth = float(w * 0.9f) / float(text.size());
+	
+	float characterX = x + w / 20.0f;
+
+	for(auto c : text)
+	{
+		const float3 wh = font.wh[c];
+		const float scale = wh.x / wh.y;
+		const SDL_Rect characterRect = { characterX, y + w / 10.0f + scale * wh.z + scale * wh.y, textCharacterWidth, textCharacterWidth * scale };
+		characterX += textCharacterWidth;
+
+		SDL_RenderCopy(gRenderer, font.textures[c], nullptr, &characterRect);
+	}
+}
+
+void MenuState(SDL_GameController* controller1, GameState& state)
+{
+	const static SDL_Color palette[] = {
+		{ 0x55, 0x49, 0x94 },
+		{ 0xF6, 0x75, 0xA8 },
+		{ 0xF2, 0x93, 0x93 },
+		{ 0xFF, 0xCC, 0xB3 },
+	};
+
+	int menuSelection = 0;
+
+	bool up_Button_prev		= false;
+	bool down_Button_prev	= false;
+
+	while (true)
+	{
+		for (SDL_Event event; SDL_PollEvent(&event););
+
+		SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
+		SDL_RenderClear(gRenderer);
+
+		const int buttonWidth 	= SCREEN_WIDTH / 5;
+		const int buttonHeight 	= 100;
+		
+		DrawButton(
+			SCREEN_WIDTH / 2 - buttonWidth / 2, SCREEN_HEIGHT / 5 * 1, buttonWidth, buttonHeight, "Play", 
+			palette[menuSelection == 0 ? 0 : 3], state.defaultFont);
+		DrawButton(SCREEN_WIDTH / 2 - buttonWidth / 2, SCREEN_HEIGHT / 5 * 2, buttonWidth, buttonHeight, "Quit", 
+			palette[menuSelection == 1 ? 0 : 3], state.defaultFont);
+
+		SDL_RenderPresent(gRenderer);
+
+		const bool up_Button	= SDL_GameControllerGetButton(controller1, SDL_CONTROLLER_BUTTON_DPAD_UP) != 0;
+		const bool down_Button	= SDL_GameControllerGetButton(controller1, SDL_CONTROLLER_BUTTON_DPAD_DOWN) != 0;
+		const bool x_Button 	= SDL_GameControllerGetButton(controller1, SDL_CONTROLLER_BUTTON_A) != 0;
+
+		if(up_Button && !up_Button_prev)
+		{
+			printf("Up_Button down \n");
+			menuSelection = Clamp(0, menuSelection + 1, 1);
+		}
+		if(down_Button && !down_Button_prev)
+		{
+			printf("Down_Button down \n");
+			menuSelection = Clamp(0, menuSelection - 1, 1);
+		}
+
+		up_Button_prev = up_Button;
+		down_Button_prev = down_Button;
+
+		if(x_Button)
+		{
+			switch(menuSelection)
+			{
+				case 0:
+					state.mode = GameMode::Game;
+					return;
+					break;
+				case 1:
+					SDL_Quit();
+					sceKernelExitProcess(0);
+					break;
+			}
+		}
+
+		SDL_Delay(16);
+	}
+}
+
+void VictoryState(SDL_GameController* controller1, GameState& state)
+{
+}
+
+
+int main(int argc, char *argv[]) 
+{
+	if( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER ) < 0 )
+		return -1;
+
+	if ((gWindow = SDL_CreateWindow( "RedRectangle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN)) == NULL)
+		return -1;
+
+	if ((gRenderer = SDL_CreateRenderer( gWindow, -1, 0)) == NULL)
+		return -1;
+
+	SDL_GameController* controller1 = SDL_GameControllerOpen(0);
+	printf("Hello: %i\n", (int)controller1 );
+
+	GameState state;
+	LoadFont(state);
+
+	state.mode = GameMode::Menu;
+
+	while(true)
+		switch (state.mode)
+		{
+			case GameMode::Menu:
+				MenuState(controller1, state);
+				break;
+			case GameMode::Game:
+				PlayState(controller1, state);
+				break;
+		}
 
 	SDL_DestroyRenderer( gRenderer );
 	SDL_DestroyWindow( gWindow );
